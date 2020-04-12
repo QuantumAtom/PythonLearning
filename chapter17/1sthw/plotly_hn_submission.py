21feea53407e200752d4514526ff81e3aa967120
QuantumAtom <zixowe@gmail.com>
2020-04-12 14:29:45 -0500

    Update 4-12-2020

--- chapter17/1sthw/plotly_hn_submission.py
+++ chapter17/1sthw/plotly_hn_submission.py
@@ -62,7 +62,21 @@ app.layout = html.Div(children=[
         id='Hacker News Enthusiasm',
         figure={
             go.figure([go.Bar(x=submission_dicts.title, y=submission_dicts.comments)])
-			fig.showl
+			fig.show
+            }
+        }
+    )
+])
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
             }
         }
     )
